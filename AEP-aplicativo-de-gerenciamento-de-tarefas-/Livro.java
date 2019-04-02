a0b5689d9fd6268d5155e823e66c375dd17ab2bd
Jhonatan Camargo <30507220+Jhownalizando@users.noreply.github.com>
2019-04-01 22:02:34 -0300

    Add files via upload

---
+++ AEP-aplicativo-de-gerenciamento-de-tarefas-/Livro.java
@@ -0,0 +1,51 @@
+package Entidades;
+
+
+public class Livro {public class Livro {
+    @Id
+    private Long id;
+    private String title;
+    private String author;
+    private int pages;
+    
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    
+    
+}
+
+
+}
---
+++ AEP-aplicativo-de-gerenciamento-de-tarefas-/src/Entidades/LivroRestServiceApplication.java
@@ -0,0 +1,13 @@
+package Entidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroRestServiceApplication.class, args);
+	}
+
+}
